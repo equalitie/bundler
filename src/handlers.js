ce0c9b7bcb20b8bdc8c2f84b05334539a1eabe2c
Zack Mullaly <redwire@riseup.net>
2015-02-06 21:14:38 -0330

    Moving now reusable handler functionality to helpers module

--- src/handlers.js
+++ src/handlers.js
@@ -10,84 +10,22 @@
  * a series of handler calls.
  */
 
-var urllib = require('url');
-var async = require('async');
-var _ = require('lodash');
-var cheerio = require('cheerio');
 var log = require('./logger');
 var helpers = require('./helpers');
-
-function htmlFinder(source, selector, attr) {
-  var $ = cheerio.load(source);
-  return function (callback) {
-    $(selector).each(function (index, elem) {
-      var $_this = $(this);
-      var resource = $_this.attr(attr);
-      if (typeof resource !== 'undefined') {
-        log.debug('Calling callback for handling selector %s', selector);
-        callback(resource);
-      }
-    });
-  };
-}
-
-function replaceAll(request, url, finder, callback) {
-  // Prepare functions for parallel invokation.
-  var elementHandlers = [];
-  finder(function (resource) {
-    elementHandlers.push(function (asynccallback) {
-      makeDiff(request, url, resource, function (err, response, diff) {
-        // TODO - Implement post-resource hooks and call them here.
-        // What would they actually do?
-        asynccallback(err, diff);
-      });
-    });
-  });
-  log.info('Found %d resources in %s', elementHandlers.length, url);
-  async.parallel(elementHandlers, function (err, diffs) {
-    if (err) {
-      callback(err, {});
-    } else {
-      var allDiffs = {};
-      for (var i = 0, len = diffs.length; i < len; ++i) {
-        allDiffs = _.extend(allDiffs, diffs[i]);
-      }
-      log.debug('Type of allDiffs = %s', typeof allDiffs);
-      log.debug(Object.keys(allDiffs)[0]);
-      callback(null, allDiffs);
-    }
-  });
-}
-
-function makeDiff(request, baseURL, resource, callback) {
-  var resourceURL = urllib.resolve(baseURL, resource);
-  var options = { url: resourceURL, encoding: null };
-  request(options, function (err, response, body) {
-    if (err) {
-      log.error('Failed to fetch %s. Error: %s', resourceURL, err.message);
-      callback(err, response, {});
-    } else {
-      var datauri = helpers.dataURI(resourceURL, body);
-      var diff = {};
-      diff[resource] = datauri;
-      callback(null, response, diff);
-    }
-  });
-}
 
 module.exports = {
   replaceImages: function (request, originalDoc, url, callback) {
     log.debug('Calling replaceImages handler');
-    replaceAll(request, url, htmlFinder(originalDoc, 'img', 'src'), callback);
+    helpers.replaceAll(request, url, helpers.htmlFinder(originalDoc, 'img', 'src'), callback);
   },
 
   replaceCSSFiles: function (request, originalDoc, url, callback) {
     log.debug('Calling replaceCSSFiles handler');
-    replaceAll(request, url, htmlFinder(originalDoc, 'link[rel="stylesheet"]', 'href'), callback);
+    helpers.replaceAll(request, url, helpers.htmlFinder(originalDoc, 'link[rel="stylesheet"]', 'href'), callback);
   },
 
   replaceJSFiles: function (request, originalDoc, url, callback) {
     log.debug('Calling replaceJSFiles handler');
-    replaceAll(request, url, htmlFinder(originalDoc, 'script', 'src'), callback);
+    helpers.replaceAll(request, url, helpers.htmlFinder(originalDoc, 'script', 'src'), callback);
   }
 };
--- src/helpers.js
+++ src/helpers.js
@@ -5,8 +5,13 @@
  * Functions made to assist in writing handlers and hooks.
  */
 
+var urllib = require('url');
+var async = require('async');
+var _ = require('lodash');
+var cheerio = require('cheerio');
 var mime = require('mime');
 var css = require('css');
+var log = require('./logger');
 
 module.exports = {
   mimetype: function (url) {
@@ -28,6 +33,20 @@ module.exports = {
     return 'data:' + this.mimetype(url) + ';base64,' + encoded;
   },
 
+  htmlFinder: function (source, selector, attr) {
+    var $ = cheerio.load(source);
+    return function (callback) {
+      $(selector).each(function (index, elem) {
+        var $_this = $(this);
+        var resource = $_this.attr(attr);
+        if (typeof resource !== 'undefined') {
+          log.debug('Calling callback for handling selector %s', selector);
+          callback(resource);
+        }
+      });
+    };
+  },
+
   cssReferenceFinder: function (source) {
     var parseTree = css.parse(source);
     return function (callback) {
@@ -44,5 +63,49 @@ module.exports = {
         }
       }
     };
+  },
+
+  replaceAll: function (request, url, finder, callback) {
+    // Prepare functions for parallel invokation.
+    var elementHandlers = [];
+    finder(function (resource) {
+      elementHandlers.push(function (asynccallback) {
+        this.makeDiff(request, url, resource, function (err, response, diff) {
+          // TODO - Implement post-resource hooks and call them here.
+          // What would they actually do?
+          asynccallback(err, diff);
+        });
+      });
+    });
+    log.info('Found %d resources in %s', elementHandlers.length, url);
+    async.parallel(elementHandlers, function (err, diffs) {
+      if (err) {
+        callback(err, {});
+      } else {
+        var allDiffs = {};
+        for (var i = 0, len = diffs.length; i < len; ++i) {
+          allDiffs = _.extend(allDiffs, diffs[i]);
+        }
+        log.debug('Type of allDiffs = %s', typeof allDiffs);
+        log.debug(Object.keys(allDiffs)[0]);
+        callback(null, allDiffs);
+      }
+    });
+  },
+
+  makeDiff: function (request, baseURL, resource, callback) {
+    var resourceURL = urllib.resolve(baseURL, resource);
+    var options = { url: resourceURL, encoding: null };
+    request(options, function (err, response, body) {
+      if (err) {
+        log.error('Failed to fetch %s. Error: %s', resourceURL, err.message);
+        callback(err, response, {});
+      } else {
+        var datauri = this.dataURI(resourceURL, body);
+        var diff = {};
+        diff[resource] = datauri;
+        callback(null, response, diff);
+      }
+    });
   }
 };
